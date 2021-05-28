95791faa6bc89007cc8c6b6448d154fa18bc0924
Tharinee Thuengnok <tharinee_t@cmu.ac.th>
2021-05-28 16:44:08 +0700

    05 alert

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
+document.getElementById('no').addEventListener('click', function(e) {
+    alert('No Successful!')
+}
+)
+
 document.getElementById('cancel').addEventListener('mousemove', (event) => {
 	console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
 });
